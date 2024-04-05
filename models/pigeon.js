531d11ca666aec4e101b63e6bd1d214674f67d17
Sem Stassen <S568124@nwmissouri.edu>
2024-04-05 16:47:11 -0500

    update search results view

--- models/pigeon.js
+++ models/pigeon.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const pigeonSchema = mongoose.Schema({
-price: Number,
-breed: String,
-gender: ["male", "female"]
+  price: {
+    type: Number,
+    min: [0, "Price can't be negative"],
+    max: [999999, "Price can't be more than a million"],
+  },
+  breed: String,
+  gender: ["male", "female"],
 });
 
 module.exports = mongoose.model("Pigeon", pigeonSchema);
