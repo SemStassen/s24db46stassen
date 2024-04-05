685f126a207c0d49ac76cb51a1afa376d88c772b
Sem Stassen <S568124@nwmissouri.edu>
2024-04-05 16:31:15 -0500

    add auth

--- routes/pigeons.js
+++ routes/pigeons.js
@@ -2,6 +2,14 @@ var express = require("express");
 const pigeon_controller = require("../controllers/pigeons");
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+};
+
 /* GET home page. */
 router.get("/", pigeon_controller.pigeon_view_all_page);
 
@@ -11,7 +19,7 @@ router.get("/detail", pigeon_controller.pigeon_view_one_page);
 // GET create costume page
 router.get("/create", pigeon_controller.pigeon_create_page);
 // GET create update page
-router.get("/update", pigeon_controller.pigeon_update_page);
+router.get("/update", secured, pigeon_controller.pigeon_update_page);
 // GET delete costume page
 router.get("/delete", pigeon_controller.pigeon_delete_page);
 
